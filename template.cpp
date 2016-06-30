591be3815613e8a253dec2e2db48ada2de26ee81
yurahuna <mettameter@gmail.com>
2016-06-30 22:37:31 +0900

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long   // <-----!!!!!!!!!!!!!!!!!!!
+
+#define rep(i,n) for (int i=0;i<(n);i++)
+#define rep2(i,a,b) for (int i=(a);i<(b);i++)
+#define rrep(i,n) for (int i=(n)-1;i>=0;i--)
+#define rrep2(i,a,b) for (int i=(b)-1;i>=(a);i--)
+#define all(a) (a).begin(),(a).end()
+
+typedef long long ll;
+typedef pair<int, int> Pii;
+typedef tuple<int, int, int> TUPLE;
+typedef vector<int> V;
+typedef vector<V> VV;
+typedef vector<VV> VVV;
+const int inf = 1e9;
+const int mod = 1e9 + 7;
