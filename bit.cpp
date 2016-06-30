4a3c92a9152838543e4f1eb85e155769bdfa2ae7
yurahuna <mettameter@gmail.com>
2016-06-30 22:18:38 +0900

    Update bit.cpp

--- bit.cpp
+++ bit.cpp
@@ -1,35 +1,35 @@
 class Bit {
-            private:
-                vector<int> bit;
-                int n;
+private:
+    vector<int> bit;
+    int n;
 
-            public:
-                Bit(int _n) {
-                    n = _n;
-                    bit.clear();
-                    bit.resize(n + 1, 0);
-                }
+public:
+    Bit(int _n) {
+        n = _n;
+        bit.clear();
+        bit.resize(n + 1, 0);
+    }
 
-                // get sum in [1, i]
-                // sum{[i, j]} = sum{[1, j]} - sum{[1, i-1]}
-                int sum(int i) {
-                    int s = 0;
-                    while (i > 0) {
-                        s += bit[i];
-                        i -= i & -i;
-                    }
-                    return s;
-                }
+    // get sum in [1, i]
+    // sum{[i, j]} = sum{[1, j]} - sum{[1, i-1]}
+    int sum(int i) {
+        int s = 0;
+        while (i > 0) {
+            s += bit[i];
+            i -= i & -i;
+        }
+        return s;
+    }
 
-                int sum(int l, int r) {
-                    return sum(r) - sum(l - 1);
-                }
+    int sum(int l, int r) {
+        return sum(r) - sum(l - 1);
+    }
 
-                // add x to bit[i]
-                void add(int i, int x) {
-                    while (i <= n) {
-                        bit[i] += x;
-                        i += i & -i;
-                    }
-                }
-            };
+    // add x to bit[i]
+    void add(int i, int x) {
+        while (i <= n) {
+            bit[i] += x;
+            i += i & -i;
+        }
+    }
+};
